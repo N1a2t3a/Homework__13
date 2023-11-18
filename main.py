ed440bb7648b11bd33765c013eb693a005029c44
Natalia <semnatalis52@gmail.com>
2023-11-18 22:22:37 +0200

    corrected Homework__13

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, APIRouter, Depends, HTTPException
 from typing import List, Optional
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models import Contact, Base
 from . import crud, schemas
-from .database import get_db
-from sqlalchemy.orm import Session
+from .database import get_db, engine
 
+# Створення основного екземпляру FastAPI
+app = FastAPI()
+
+# Створення роутерів
 router = APIRouter()
 
 @router.get("/search/")
@@ -16,14 +19,6 @@ async def search_contacts(query: str, db: Session = Depends(get_db)):
     if not contacts:
         raise HTTPException(status_code=404, detail="Contacts not found")
     return contacts
-
-# Створення бази даних та таблиці контактів
-DATABASE_URL = connect(db="mydatabase", host="localhost")
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
 
 # Схема Pydantic для створення контакту
 class ContactCreate(BaseModel):
@@ -35,58 +30,30 @@ class ContactCreate(BaseModel):
     additional_data: Optional[str] = None
 
 # Роут для створення нового контакту
-@app.post("/contacts/", response_model=Contact)
-def create_contact(contact: ContactCreate):
-    db = SessionLocal()
-    db_contact = Contact(**contact.dict())
-    db.add(db_contact)
-    db.commit()
-    db.refresh(db_contact)
-    db.close()
-    return db_contact
+@app.post("/contacts/", response_model=schemas.Contact)
+def create_contact(contact: ContactCreate, db: Session = Depends(get_db)):
+    return crud.create_contact(db=db, contact=contact)
 
 # Роут для отримання списку всіх контактів
-@app.get("/contacts/", response_model=List[Contact])
-def read_contacts(skip: int = 0, limit: int = 10):
-    db = SessionLocal()
-    contacts = db.query(Contact).offset(skip).limit(limit).all()
-    db.close()
-    return contacts
+@app.get("/contacts/", response_model=List[schemas.Contact])
+def read_contacts(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_contacts(db, skip=skip, limit=limit)
 
 # Роут для отримання контакту за ідентифікатором
-@app.get("/contacts/{contact_id}", response_model=Contact)
-def read_contact(contact_id: int):
-    db = SessionLocal()
-    contact = db.query(Contact).filter(Contact.id == contact_id).first()
-    db.close()
-    if contact is None:
-        raise HTTPException(status_code=404, detail="Contact not found")
-    return contact
+@app.get("/contacts/{contact_id}", response_model=schemas.Contact)
+def read_contact(contact_id: int, db: Session = Depends(get_db)):
+    return crud.get_contact(db, contact_id=contact_id)
 
 # Роут для оновлення контакту за ідентифікатором
-@app.put("/contacts/{contact_id}", response_model=Contact)
-def update_contact(contact_id: int, contact: ContactCreate):
-    db = SessionLocal()
-    db_contact = db.query(Contact).filter(Contact.id == contact_id).first()
-    if db_contact is None:
-        db.close()
-        raise HTTPException(status_code=404, detail="Contact not found")
-    for field, value in contact.dict().items():
-        setattr(db_contact, field, value)
-    db.commit()
-    db.refresh(db_contact)
-    db.close()
-    return db_contact
+@app.put("/contacts/{contact_id}", response_model=schemas.Contact)
+def update_contact(contact_id: int, contact: ContactCreate, db: Session = Depends(get_db)):
+    return crud.update_contact(db=db, contact_id=contact_id, contact=contact)
 
 # Роут для видалення контакту за ідентифікатором
-@app.delete("/contacts/{contact_id}", response_model=Contact)
-def delete_contact(contact_id: int):
-    db = SessionLocal()
-    contact = db.query(Contact).filter(Contact.id == contact_id).first()
-    if contact is None:
-        db.close()
-        raise HTTPException(status_code=404, detail="Contact not found")
-    db.delete(contact)
-    db.commit()
-    db.close()
-    return contact
+@app.delete("/contacts/{contact_id}", response_model=schemas.Contact)
+def delete_contact(contact_id: int, db: Session = Depends(get_db)):
+    return crud.delete_contact(db=db, contact_id=contact_id)
+
+# Додавання роутера до основного екземпляру FastAPI
+app.include_router(router, prefix="/api/v1")
+
